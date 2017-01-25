b7cdaa961aa79bb818e50cc54c76f06f0e178f42
Kaitlin Flynn <kaitlinflynn@lesniak-imac.local>
2017-01-25 11:34:44 -0500

    nmds comparisons

--- code/diversity_analysis.R
+++ code/diversity_analysis.R
@@ -2,12 +2,12 @@
 #Kaitlin 1 24
 
 #bring in files
-metadata <- read.table(file='data/raw/kws_metadata.tsv', header = T)
+meta_file <- read.table(file='data/raw/kws_metadata.tsv', header = T)
 #simpson <- read.table(file='kws.an.0.03.subsample.groups.summary', header = T)
 nmds <- read.table(file='data/mothur/kws_final.an.thetayc.0.03.lt.ave.nmds.axes', header = T)
-#shannon <- read.table(file='kws.an.groups.summary', header = T)
+simps <- read.table(file='data/mothur/kws_final.an.groups.summary', header = T)
 #fullshan <- merge(shannon, metadata)
-
+shared_file <- read.table(file='data/mothur/kws_final.an.shared', sep = '\t', header=T, row.names=2)
 
 #load niel's script for properly , reading in thetayc distances
 #source(file = 'read.dist.R')
@@ -26,18 +26,61 @@ plot(nmds$axis1, nmds$axis2)
 
 nmds <- merge(nmds, metadata)
 
+sharedmet <- merge(shared_file, meta_file, by.x="row.names", by.y='group')
+
+
+#ok this is looking better. if can make for all sides/comparisons, then can determine if donors are closer to each other or locations. actually probably need to subset shared first, then do nmds each time 
+leftside <- subset(nmds, side=='left')
+rightsidenmds <- subset(nmds, side=='right')
+
+ggplot(leftside, aes(x=axis1, y=axis2)) + geom_point(aes(color=as.factor(patient), shape=site, size = 2)) +theme_bw() + ggtitle("Left colon comparison")
+
+ggplot(rightsidenmds, aes(x=axis1, y=axis2)) + geom_point(aes(color=as.factor(patient), shape=site, size = 2)) +theme_bw() + ggtitle("Right colon comparison")
+
+#subset shared and then do distance, nmds to get comparisons of just those samples 
+
+left <- meta_file$group[meta_file$side=='left']
+leftshared <- shared_file[row.names(shared_file) %in% left,]
+leftshared <- add_rownames(leftshared, var = "group")
+leftshared <- leftshared[-2]
+leftshared <- cbind(label=0.03, leftshared[1:ncol(leftshared)])
+
+right <- meta_file$group[meta_file$side=='right']
+rightshared <- shared_file[row.names(shared_file) %in% right,]
+rightshared <- add_rownames(rightshared, var = "group")
+rightshared <- rightshared[-2]
+rightshared <- cbind(label=0.03, rightshared[1:ncol(rightshared)])
+
+mucosa <- meta_file$group[meta_file$site=='mucosa']
+mucosashared <- shared_file[row.names(shared_file) %in% mucosa,]
+mucosashared <- add_rownames(mucosashared, var = "group")
+mucosashared <- mucosashared[-2]
+mucosashared <- cbind(label=0.03, mucosashared[1:ncol(mucosashared)])
+
+stool <- meta_file$group[meta_file$site=='stool']
+stoolshared <- shared_file[row.names(shared_file) %in% stool,]
+stoolshared <- add_rownames(stoolshared, var = "group")
+stoolshared <- stoolshared[-2]
+stoolshared <- cbind(label=0.03, stoolshared[1:ncol(stoolshared)])
+
+
+write.table(leftshared, file='data/process/leftshared.shared', sep='\t', quote=F, row.names=F)
+write.table(rightshared, file='data/process/rightshared.shared', sep='\t', quote=F, row.names=F)
+write.table(mucosashared, file='data/process/mucosashared.shared', sep='\t', quote=F, row.names=F)
+write.table(stoolshared, file='data/process/stoolshared.shared', sep='\t', quote=F, row.names=F)
+
 #nmds plot of distances faceted by location
 ggplot(nmds, aes(axis1, axis2)) +geom_point(aes(color=patient, shape=side)) + facet_wrap(~location)
 
 #color points by organ 
-points(left_mucosa, pch=16, col = "blue")
-points(right_mucosa, pch=16, col = "red")
-points(left_stool, pch=16, col = "green")
-points(right_stool, pch=16, col = "orange")
-points(spon_stool, pch=16, col = "purple")
+points(left_mucosa, pch=nmds$patient, col = "blue")
+points(right_mucosa, pch=nmds$patient,col = "red")
+points(left_stool, pch=nmds$patient, col = "green")
+points(right_stool, pch=nmds$patient, col = "orange")
+points(spon_stool, pch=nmds$patient, col = "purple")
 
 legend <- c("left_mucosa", "right_mucosa", "left_stool", "right_stool", "spon_stool")
-legend(x="topright", legend, col = c("blue", "red", "green", "orange", "purple"), pch=16)
+legend(x="topright", legend, col = c("blue", "red", "green", "orange", "purple"), pch=nmds$patient)
 
 #lets do a test to compare 3 patients patients, or within one patient
 #or subset just to test three patients
@@ -118,7 +161,7 @@ labels <- c("LM/LM", "LM/RM", "RM/RM", "LS/LS", "LS/RS", "RS/RS", "LS/SS", "RS/S
 boxplot(left2left, left2right, right2right, LS2LS, LS2RS, RS2RS, LS2SS, RS2SS, SS2SS, main="dissimilarity between sample sites, all patients", ylab="theta YC distance", names=labels)
 
 ## now do simpson diversity per patient
-metasimp <- merge(metadata, simpson)
+metasimp <- merge(metadata, simps)
 
 #want to plot diversity side by side for each patient.
 #plotted with a loop
